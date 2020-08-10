eca781e4ce3c3bbd81ca16a368958455197b9066
sebasFili <sebastianfilippelli@gmail.com>
2020-08-09 21:17:33 -0300

    agregado de link para menú y ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DetalleComponent } from './detalle/detalle.component';
 import { RegistroComponent } from './registro/registro.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'detalle', component: DetalleComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'catalogo', component: CatalogoComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
